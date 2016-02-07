8907b0bc274286b66402877fc3f92619a335efb8
Crystal Prieb <cjprieb@gmail.com>
2016-02-06 18:09:07 -0600

    Checkboxes are styled green and use font-awesome's check icon

--- client/templates/checkbox-button.js
+++ client/templates/checkbox-button.js
@@ -1,3 +1,6 @@
-Template.checkboxButton.helpers({
-    
-});
+Template.checkboxButton.rendered = function () {
+    var customCheckbox = this.$(".checkbox-custom");
+    this.$("[type=checkbox]").on("change", function() {
+        customCheckbox.toggleClass("fa-check", this.checked, 0);        
+    });
+}
